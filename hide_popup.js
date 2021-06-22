4ff10b0ad600eb495acb6e2a379477deb336e918
DuangXT <154174478@qq.com>
2021-06-22 16:58:50 +0800

    添加三大妈等网站内容优化

--- hide_popup.js
+++ hide_popup.js
@@ -1,12 +1,17 @@
 // ==UserScript==
 // @name         隐藏网站干扰项
 // @description  包括【自动跳过bilibili充电鸣谢】、【自动隐藏bilibili大会员解锁高画质弹窗】、【自动隐藏CSDN登录弹窗】、【自动隐藏知乎登录弹窗】
-// @version      0.0.3
+// @version      0.0.4
 // @author       DuangXT
 // @namespace    https://github.com/DuangXT/some_monkeys_js
-// @match        *.bilibili.com/video/*
-// @match        *.zhihu.com/*
-// @match        *.csdn.net/*
+// @match        *://*.bilibili.com/video/*
+// @match        *://*.zhihu.com/*
+// @match        *://*.csdn.net/*
+// @match        *://jamcz.com/*
+// @match        *://dl.3dmgame.com/patch/*
+// @match        *://mod.3dmgame.com/mod/*
+// @match        *://blzxteam.com/*
+// @match        *://*.jianshu.com/p/*
 // @include      *
 // @homepageURL  https://github.com/DuangXT/some_monkeys_js
 // @updateURL    https://raw.githubusercontent.com/DuangXT/some_monkeys_js/main/hide_popup.js
@@ -17,16 +22,19 @@
 
 (function() {
     window.onload = function(){
-        // 链接
-        let currentURL = window.location.href; // 获取当前网页地址
-        let domainURL = currentURL.split("//")[1].split("/")[0];
-        let hostURL = domainURL.split('.').length == 3 ?
-            domainURL.split('.')[1] + '.' + domainURL.split('.')[2]
-            : domainURL; // 只处理两个根，其它多的不考虑
+        let href = window.location.href; // 获取当前网页地址
+        console.log("[隐藏网站干扰项]开始，当前页面链接：" + href);
 
-        console.log("[隐藏网站干扰项]开始，当前页面链接：" + currentURL);
+        if(urlMatching(href, "jamcz.com/")){
+            document.querySelectorAll();
+            let ass = document.querySelectorAll("a[target=_blank]");
+            for (let a of ass) {
+                if(a.innerText.indexOf("网盘") >=0 && a.onclick)
+                    a.onclick = a.onclick.replace('jq(','window.open(');
+            }
+        }
 
-        if(urlMatching(currentURL, "dl.3dmgame.com/patch/")){ // 3DM资源下载
+        if(urlMatching(href, "dl.3dmgame.com/patch/")){ // 3DM资源下载
             document.querySelector("#layui-layer-shade1").remove();
             let els = document.querySelectorAll('.dllist>a');
             for (let a of els) {
@@ -34,14 +42,14 @@
                 a.removeAttribute("onclick");
             }
         }
-        if(urlMatching(currentURL, "mod.3dmgame.com/mod/")){ // 3DM模组下载
+        if(urlMatching(href, "mod.3dmgame.com/mod/")){ // 3DM模组下载
             document.querySelector('.not-ad.mod-download-btn.openBox')
                 .setAttribute("onclick", "window.open('" +
                     document.querySelector('.mod-download-btn.col-xs-12>a').href + "');");
             document.querySelector('.mod-download-window').remove();
         }
 
-        if(urlMatching(currentURL, "blzxteam.com")){ // 深海迷航社区
+        if(urlMatching(href, "blzxteam.com")){ // 深海迷航社区
             // 删除公告窗
             let w = document.querySelector("#layui-layer1.layui-layer.layui-layer-dialog");
             let s = document.querySelector("#layui-layer-shade1.layui-layer-shade");
@@ -52,7 +60,7 @@
             document.querySelector("#myhkplayer.playing").remove();
         }
 
-        if(urlMatching(currentURL, ".bilibili.com/video/")){
+        if(urlMatching(href, ".bilibili.com/video/")){
             run(function(){
                 document.querySelector('video').onended=()=>{
                     document.querySelector('.bilibili-player-electric-panel-jump-content').click();
@@ -69,7 +77,7 @@
             }, "隐藏播放中在视频内弹出的 +关注、投票 等功能按钮 时发生异常：", "已隐藏视频中高级功能按钮");
         }
 
-        if(urlMatching(currentURL, ".zhihu.com/")){
+        if(urlMatching(href, ".zhihu.com/")){
             run(function(){
                 // 隐藏登录框
                 if(document.querySelector(".Modal-wrapper"))
@@ -77,7 +85,7 @@
             }, "隐藏【知乎登录弹框】时异常：", "已隐藏【知乎登录弹框】");
         }
 
-        if(urlMatching(currentURL, ".csdn.net/")){
+        if(urlMatching(href, ".csdn.net/")){
             run(function(){
                 if(document.querySelector('#csdn-redpack'))
                     document.querySelector('#csdn-redpack').remove();
@@ -95,7 +103,7 @@
             }, "隐藏【CSDN登录弹框】时异常：", "已隐藏【CSDN登录弹框】");
         }
 
-        if(urlMatching(currentURL, ".jianshu.com/p/")){
+        if(urlMatching(href, ".jianshu.com/p/")){
             // 简书页面一个奇怪的抽奖广告
             if(document.querySelector('._1aCo37-mask')){
                 document.querySelector('._1aCo37-mask').setAttribute("style",style_hidden);
