56d5b03e7f3303df6fd1023d383d72217c0166b2
DuangXT <154174478@qq.com>
2023-05-01 10:02:43 +0800

    #

--- common.user.js
+++ common.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         个人常用js脚本方法、参数
 // @description  避免总是复制粘贴的东西
-// @version      0.0.5
+// @version      0.0.5.1
 // @author       DuangXT
 // @grant        none
 // @match        *
