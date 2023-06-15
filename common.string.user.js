ad6d4a46fd9a3c75a9e559670e85669b033695aa
DuangXT <154174478@qq.com>
2023-06-15 18:22:08 +0800

    update

--- common.string.user.js
+++ common.string.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         常用油猴脚本方法：字符串
 // @description
-// @version      0.0.4
+// @version      0.0.5
 // @author       DuangXT
 // @homepageURL  https://github.com/DuangXT/some_monkeys_js/
 // @updateURL    https://raw.githubusercontent.com/DuangXT/some_monkeys_js/main/common.string.user.js
@@ -14,29 +14,9 @@
 // common level 0
 gmlog('公共库：字符串');
 
-String.prototype.contains = function (...strings) {
-    for (let s of strings) {
-        s = s.toString();
-        if(
-            // this.indexOf(s) >= 0 // <ES6
-            this.includes(s) // >=ES6
-        ) return true;
-    }
-    return false;
-}
-
-String.prototype.notContains = !String.prototype.contains;
-String.prototype.containsIgnoreCase = function (...substrs){
-    let newSubStrs = [];
-    substrs.forEach(s=> newSubStrs.push(s.toUpperCase()));
-    return this.toUpperCase().contains(...newSubStrs);
-}
-String.prototype.notContainsIgnoreCase = !String.prototype.containsIgnoreCase;
-
 String.prototype.equals = function(...string){
     for (const s of string) {
-        if (this === s)
-            return true;
+        if (this === s) return true;
     }
     return false;
 }
@@ -48,6 +28,27 @@ String.prototype.equalsIgnoreCase = function(...string){
     return false;
 }
 
+const strEquals = (s1, s2, ignoreCase=false) =>
+    ignoreCase && s1 && s2 ? s1.toUpperCase() === s2.toUpperCase() : s1 === s2;
+
+String.prototype.contains = function (...strings) {
+    for (let s of strings) {
+        s = s.toString();
+        if(
+            // this.indexOf(s) >= 0 // <ES6
+            this.includes(s) // >=ES6
+        ) return true;
+    }
+    return false;
+}
+
+String.prototype.notContains=(...s)=>!String.prototype.contains(...s);
+String.prototype.containsIgnoreCase = function (...substrs){
+    let newSubStrs = [];
+    substrs.forEach(s=> newSubStrs.push(s.toUpperCase()));
+    return this.toUpperCase().contains(...newSubStrs);
+}
+String.prototype.notContainsIgnoreCase=(...s)=>!String.prototype.containsIgnoreCase(...s);
 
 /** 查找字符串中是否包含指定字符 */
 const strContains = (str, ...substrs) => {
