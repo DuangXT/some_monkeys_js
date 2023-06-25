385b1836a257e6728fbddd3b32a0eac6931dd58c
DuangXT <154174478@qq.com>
2023-06-26 02:45:08 +0800

    update

--- common.window.user.js
+++ common.window.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name 常用油猴脚本方法：网页窗口
-// @version 0.0.1
+// @version 0.0.3
 // @author DuangXT
 // @require https://raw.githubusercontent.com/DuangXT/some_monkeys_js/main/common.log.user.js
 // @require https://raw.githubusercontent.com/DuangXT/some_monkeys_js/main/common.string.user.js
@@ -8,11 +8,11 @@
 // common level 0
 gmlog('公共库：网页窗口');
 
-/** 关闭当前窗口 */
-const closeWindow = closenow = closewindow = ()=>{
+/** 立即关闭当前窗口 */
+const closenow = ()=>{
     try{
+        log("将要关闭当前窗口", location.href);
         // window.open(location, "_self").close();
-
         if (navigator.userAgent.containsIgnoreCase('Firefox', 'Chrome')) {
             // 重定向到空白页再关闭
             location.href = 'about:blank'
@@ -27,3 +27,8 @@ const closeWindow = closenow = closewindow = ()=>{
         logerror("关闭当前窗口出错", e);
     }
 }
+
+/** 关闭当前窗口 */
+const closeWindow = closewindow = (timeout=0)=> setTimeout(closenow, timeout);
+
+
