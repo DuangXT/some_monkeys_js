26c4ccac5825bd828da8bc04eda4adcc9b4814bc
DuangXT <154174478@qq.com>
2023-07-16 15:58:36 +0800

    udpate

--- common.log.user.js
+++ common.log.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name 常用油猴脚本方法：日志
-// @version 0.0.6
+// @version 0.0.7
 // @author DuangXT
 // @grant GM_log
 // @grant GM.log
 // ==/UserScript==
 // common level 0
 var log = log ? log :
-    GM_log ? GM_log : GM.log ? GM.log :
+    // GM_log ? GM_log : GM.log ? GM.log :
     (...s) => console.log.bind(console)(...s);
 const gmlog = title => log('...... 油猴脚本加载：%s ......', title);
 gmlog('公共库：日志');
